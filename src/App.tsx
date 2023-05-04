95eeb7986846f90ff2a55ab2d0f5f4b8c8f34802
Julian Toscani <julian.toscani@grapes.de>
2023-05-04 17:42:19 +0200

    text and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, Typography } from '@mui/material'
 import './App.css'
+import {useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typography variant="h3" component="h1">
+        Hello World!
+      </Typography>
+      <Typography sx={{ margin: "1rem 0"}} component="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nostrum facilis quos placeat asperiores minus aperiam, animi dicta nisi ipsa numquam impedit laborum sapiente ducimus. Itaque saepe dignissimos quia. Reiciendis?</Typography>
+      <Button variant="contained" size="large" onClick={() => setCount(count +1)}>
+        Click me!
+      </Button>
+
+      <Typography>
+        {count}
+      </Typography>
     </>
   )
 }
